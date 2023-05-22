5ac634e7005a378069d19317cc4e5e0171154caf
Manoj Paudel <109033830+sudomanoj@users.noreply.github.com>
2023-05-22 19:02:20 +0545

    Update settings.py

--- MeroShopping/settings.py
+++ MeroShopping/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s()6cckr8evg1*eyvh&6y@rc9!b2u+l1f#8e+^u0kyw=m$81)h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
